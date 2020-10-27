cc6aba163bb9129b330b23906e0e3f5619c5c421
HHxx66 <5521962+HHxx66@user.noreply.gitee.com>
2020-10-27 17:00:19 +0800

    hw5

---
+++ hw5/Assets/Scripts/CCFlyAction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCFlyAction : SSAction
+{
+    float gravity;
+    float speed;
+    Vector3 direction;
+    float time;
+
+    public static CCFlyAction GetSSAction(Vector3 direction, float speed){
+        CCFlyAction action = ScriptableObject.CreateInstance<CCFlyAction>();
+        action.gravity = 9.8f;
+        action.time = 0;
+        action.speed = speed;
+        action.direction = direction;
+        return action;
+    }
+
+    public override void Start()
+    {
+        
+    }
+
+    public override void Update(){
+        time+=Time.deltaTime;
+        transform.Translate(Vector3.down*gravity*time*Time.deltaTime);
+        transform.Translate(direction*speed*Time.deltaTime);
+        if (this.transform.position.y<-10){
+            this.destroy=true;
+            this.enable=false;
+            this.callback.SSActionEvent(this);
+        }
+    }
+}
---
+++ hw5/Assets/Scripts/IUserAction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+public interface IUserAction
+{
+	// void MoveBoat();
+    // void MoveRole(RoleModel roleModel);
+    void Restart();
+
+    //void Check();
+}
+
+
