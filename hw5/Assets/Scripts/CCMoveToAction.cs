b26e079854cdf9e1f363d28fe823167a7bb69484
HHxx66 <5521962+HHxx66@user.noreply.gitee.com>
2020-10-27 16:34:51 +0800

    hw5

---
+++ hw5/Assets/Scripts/CCMoveToAction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCMoveToAction : SSAction
+{
+    public Vector3 target;
+    public float speed;
+
+    public static CCMoveToAction GetSSAction(Vector3 target, float speed){
+        CCMoveToAction action=ScriptableObject.CreateInstance<CCMoveToAction>();
+        action.target=target;
+        action.speed=speed;
+        return action;
+    }
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        this.transform.localPosition=Vector3.MoveTowards(this.transform.localPosition,target,speed*Time.deltaTime);
+        if(this.gameObject==null||this.transform.localPosition==target){
+            this.destroy=true;
+            this.callback.SSActionEvent(this);
+        }
+    }
+}
---
+++ hw5/Assets/Scripts/UFOFactory.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOFactory : ClickAction
+{
+    private static UFOFactory _instance;
+
+    public GameObject UFORed_Prefab;
+    public GameObject UFOGreen_Prefab;
+    public GameObject UFOBlue_Prefab;
+    private List<UFOData> used;
+    private List<UFOData> free;
+    public FirstController controller;
+
+    private UFOFactory(){
+        used=new List<UFOData>();
+        free=new List<UFOData>();
+        UFORed_Prefab=GameObject.Instantiate<GameObject>(Resources.Load<GameObject>("UFORed"), Vector3.zero, Quaternion.identity);
+        UFOGreen_Prefab=GameObject.Instantiate<GameObject>(Resources.Load<GameObject>("UFOGreen"), Vector3.zero, Quaternion.identity);
+        UFOBlue_Prefab=GameObject.Instantiate<GameObject>(Resources.Load<GameObject>("UFOBlue"), Vector3.zero, Quaternion.identity);
+        UFORed_Prefab.SetActive(false);
+        UFOGreen_Prefab.SetActive(false);
+        UFOBlue_Prefab.SetActive(false);
+        controller=SSDirector.getInstance().currentSceneController as FirstController;
+    }
+
+    public static UFOFactory getInstance() {
+		if (_instance == null) {
+			_instance = new UFOFactory();
+		}
+		return _instance;
+	}
+
+    public GameObject GetUFO(int round){
+        GameObject ufo;
+        if(free.Count>0){
+            ufo=free[0].gameObject;
+            free.Remove(free[0]);
+        }
+        else{
+            float kind=UnityEngine.Random.Range(0f,round+1);
+            if(kind<0.5f){
+                ufo=GameObject.Instantiate<GameObject>(UFOBlue_Prefab, Vector3.zero, Quaternion.identity);
+                ufo.AddComponent<UFOData>();
+                ufo.AddComponent<BoxCollider>();
+                ufo.AddComponent<Click>();
+                ufo.GetComponent<UFOData>().score=1;
+            }
+            else if(kind<2.5f){
+                ufo=GameObject.Instantiate<GameObject>(UFOGreen_Prefab, Vector3.zero, Quaternion.identity);
+                ufo.AddComponent<UFOData>();
+                ufo.AddComponent<BoxCollider>();
+                ufo.AddComponent<Click>();
+                ufo.GetComponent<UFOData>().score=2;
+            }
+            else{
+                ufo=GameObject.Instantiate<GameObject>(UFORed_Prefab, Vector3.zero, Quaternion.identity);
+                ufo.AddComponent<UFOData>();
+                ufo.AddComponent<BoxCollider>();
+                ufo.AddComponent<Click>();
+                ufo.GetComponent<UFOData>().score=3;
+            }
+            ufo.GetComponent<Click>().setClickAction(this);
+        }
+        ufo.GetComponent<UFOData>().direction=new Vector3(UnityEngine.Random.Range(-2f,2f),1,0);
+        float level=UnityEngine.Random.Range(0f,round);
+        if(level<3){
+            ufo.GetComponent<UFOData>().speed=3.0f+round/6;
+        }
+        else if(level<6){
+            ufo.GetComponent<UFOData>().speed=4.0f+round/6;
+        }
+        else{
+            ufo.GetComponent<UFOData>().speed=6.0f+round/6;
+        }
+        level=UnityEngine.Random.Range(0f,round);
+        if(level<3){
+            ufo.GetComponent<UFOData>().scale=3;
+            ufo.GetComponent<BoxCollider>().size=new Vector3(3f,2f,3f);
+        }
+        else if(level<6){
+            ufo.GetComponent<UFOData>().scale=2;
+            ufo.GetComponent<BoxCollider>().size=new Vector3(4f,3f,4f);
+        }
+        else{
+            ufo.GetComponent<UFOData>().scale=1;
+            ufo.GetComponent<BoxCollider>().size=new Vector3(5f,4f,5f);
+        }
+        used.Add(ufo.GetComponent<UFOData>());
+        return ufo;
+    }
+
+    public void FreeUFO(GameObject ufo){
+        foreach(UFOData ufoData in used){
+            if(ufoData.gameObject.GetInstanceID()==ufo.GetInstanceID()){
+                ufo.SetActive(false);
+                free.Add(ufoData);
+                used.Remove(ufoData);
+                break;
+            }
+        }
+    }
+
+    public void FreeALL(){
+        foreach(UFOData ufoData in used){
+            ufoData.gameObject.SetActive(false);
+            free.Add(ufoData);
+            used.Remove(ufoData);
+        }
+        used.Clear();
+    }
+
+    public void DealClick(GameObject ufo){
+        controller.JudgeCallback(true,ufo.GetComponent<UFOData>().score+(int)ufo.GetComponent<UFOData>().speed-3+ufo.GetComponent<UFOData>().scale-1);
+        FreeUFO(ufo);
+    }
+}
