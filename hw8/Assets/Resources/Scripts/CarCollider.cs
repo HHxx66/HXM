67d8e6ce510989080b298991fcd21dcdf3d03a30
HHxx66 <5521962+HHxx66@user.noreply.gitee.com>
2020-11-28 22:08:28 +0800

    hw8

---
+++ hw8/Assets/Resources/Scripts/CarCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCollider : MonoBehaviour
+{
+    private float damage = 0;
+
+    public float GetDamage()
+    {
+        return damage;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        damage += 5;
+    }
+}
---
+++ hw8/Assets/Resources/Scripts/SmokeParticle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class SmokeParticle : MonoBehaviour
+{
+    private GameObject car;
+    private ParticleSystem exhaust;
+
+    void Start()
+    {
+        car = transform.parent.gameObject.transform.parent.gameObject;
+        exhaust = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        SetEmissionRate();
+        SetColor();
+    }
+
+    private void SetEmissionRate()
+    {
+        var K = 1000;
+        var emission = exhaust.emission;
+        emission.rateOverTime = K * car.GetComponent<CarController>().Revs + 5;
+    }
+
+    private void SetColor()
+    {
+        var color = exhaust.colorOverLifetime;
+        var damage = car.GetComponent<CarCollider>().GetDamage() + 2 * car.GetComponent<CarController>().Revs;
+        var gradient = new Gradient();
+        var colorKeys = new GradientColorKey[] { new GradientColorKey(Color.white, 0.0f), new GradientColorKey(new Color(214, 189, 151), 0.079f), new GradientColorKey(Color.white, 1.0f) };
+        var alphaKeys = new GradientAlphaKey[] { new GradientAlphaKey(0.0f, 0.0f), new GradientAlphaKey(damage / 255f, 0.061f), new GradientAlphaKey(0.0f, 1.0f) };
+        gradient.SetKeys(colorKeys, alphaKeys);
+        color.color = gradient;
+    }
+}
